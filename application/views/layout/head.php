be29a13bf3bc5e6c707ba32b51a5ed03ef30e1b3
Daniel Luna <dluna@aper.net>
2017-04-17 15:58:09 -0500

    google tag added

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -108,6 +108,18 @@
 			ga('send', 'pageview');
 		</script>
 	<?php endif;?>
+        
+        <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
+            new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
+            ’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,‘script’,‘dataLayer’,‘GTM-MT66XLH’);</script>
+            <!-- End Google Tag Manager -->
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src=“https://www.googletagmanager.com/ns.html?id=GTM-MT66XLH”
+            height=“0" width=“0” style=“display:none;visibility:hidden”></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
 </head>
 <body>
 <div class="backGround">
